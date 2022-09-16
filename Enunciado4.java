54aed4df9cd97f694a4c6dd773c1b87df05d60b2
Yasmin Carlôto <yasmincarlotobezerra17@gmail.com>
2022-09-15 23:20:51 -0300

    Primeiras questões (feitas por Yasmin Carlôto)

---
+++ Enunciado4.java
@@ -0,0 +1,16 @@
+package javaPoo2022;
+import java.util.Scanner;
+
+public class Enunciado4 {
+	public static void main(String[] args) {
+		float sal, novoSal;
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Qual o valor do seu salário?");
+		sal = sc.nextFloat();
+
+		novoSal = sal + (sal*25/100);
+		
+		System.out.format("Seu novo salário é R$%.2f", novoSal);
+	}
+}
